9576bb3248824e002a7852c3e2c1ccb5c3fee74f
aks <ankit.singhaniyaz@gmail.com>
2017-03-07 01:03:48 +0530

    made sidebar clickable

--- app/assets/javascripts/components/forms/sidebar.es6.jsx
+++ app/assets/javascripts/components/forms/sidebar.es6.jsx
@@ -1,19 +1,50 @@
+const FIELD_TYPES = {
+  shortAnswer: "SHORT_ANSWERS",
+  paragraph: "PARAGRAPH",
+  multipleChoice: "MCQ",
+  checkbox: "CHECKBOX",
+  dropdown: "DROPDOWN"
+};
+
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      active: 1
+      active: FIELD_TYPES.shortAnswer
     };
+
+    this.onFieldClick = this.onFieldClick.bind(this);
+  }
+
+  onFieldClick(active) {
+    console.log(active);
+    this.setState({active})
   }
 
   render () {
     return(
       <div className="ui secondary vertical pointing menu">
-        <a onClick="" className="active item">Short Answer</a>
-        <a onClick="" className="item">Paragraph</a>
-        <a onClick="" className="item">Multiple Choice</a>
-        <a onClick="" className="item">Checkboxes</a>
-        <a onClick="" className="item">Dropdown</a>
+        <a onClick={() => this.onFieldClick(FIELD_TYPES.shortAnswer)}
+          className={this.state.active === FIELD_TYPES.shortAnswer? "active item" : "item"}>
+          Short Answer
+        </a>
+        <a onClick={() => this.onFieldClick(FIELD_TYPES.paragraph)}
+          className={this.state.active === FIELD_TYPES.paragraph? "active item" : "item"}>
+          Paragraph
+        </a>
+        <a onClick={() => this.onFieldClick(FIELD_TYPES.multipleChoice)}
+          className={this.state.active === FIELD_TYPES.multipleChoice? "active item" : "item"}>
+          Multiple Choice
+        </a>
+        <a onClick={() => this.onFieldClick(FIELD_TYPES.checkbox)}
+          className={this.state.active === FIELD_TYPES.checkbox? "active item" : "item"}>
+          Checkboxes
+        </a>
+        <a onClick={() => this.onFieldClick(FIELD_TYPES.dropdown)}
+          className={this.state.active === FIELD_TYPES.dropdown? "active item" : "item"}>
+          Dropdown
+        </a>
       </div>
     )
   }
