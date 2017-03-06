bf6cbd8ba51a626fe5fb9237bdc0951c95a0e71a
aks <ankit.singhaniyaz@gmail.com>
2017-03-06 18:54:41 +0530

    add association in all models

--- app/models/field_datum.rb
+++ app/models/field_datum.rb
@@ -1,2 +1,5 @@
 class FieldDatum < ApplicationRecord
+  belongs_to :form_datum
+
+  validates :value, presence: true
 end
--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,7 @@
 class Form < ApplicationRecord
+  has_many :fields, dependent: :destroy
+  has_many :form_data, dependent: :destroy
+
+  validates :name, presence: true
+  validates :token, presence: true
 end
