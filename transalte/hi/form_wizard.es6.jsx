6a9a5b63cfc35a3372853c508fc6de0804c1cce9
Sanketkumar Karve <SanketKarve@users.noreply.github.com>
2021-03-14 12:13:49 +0530

    New translations form_wizard.es6.jsx (Hindi)

---
+++ transalte/hi/form_wizard.es6.jsx
@@ -0,0 +1,90 @@
+class FormWizard extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+
+  render () {
+    const { name, activeTab } = this.props;
+    if(name !== activeTab) return null
+
+    return (
+      <div>
+          <div className="is-clearfix">
+            <div className="is-pulled-right">
+              <label className="label">Theme</label>
+              <p className="control">
+                <span className="select">
+                  <select>
+                    <option>Flat Theme</option>
+                    <option selected ="selected">Bootstrap</option>
+                  </select>
+                </span>
+              </p>
+            </div>
+          </div>
+
+
+          <p className="control distance">
+            <input className="input is-large" type="text" placeholder="Form Name"/>
+          </p>
+
+
+          <div className="distance">
+
+
+          <FormField
+          
+          />
+
+          <FormFieldEdit
+
+          />
+
+
+
+
+            <div className="distance">
+              <p className="control has-addons">
+                <a className="button is-large" title="Short Answer">
+                  <span className="icon is-medium">
+                    <i className="fa fa-font"></i>
+                  </span>
+                </a>
+
+                <a className="button is-large" title="Paragraph">
+                  <span className="icon is-medium">
+                    <i className="fa fa-align-left"></i>
+                  </span>
+                </a>
+
+                <a className="button is-large" title="Multiple Choice">
+                  <span className="icon is-medium">
+                    <i className="fa fa-check-square"></i>
+                  </span>
+                </a>
+
+                <a className="button is-large" title="Checkboxes">
+                  <span className="icon is-medium">
+                    <i className="fa fa-dot-circle-o"></i>
+                  </span>
+                </a>
+
+                <a className="button is-large" title="Dropdown">
+                  <span className="icon is-medium">
+                    <i className="fa fa-chevron-circle-down"></i>
+                  </span>
+                </a>
+              </p>
+            </div>
+          </div>
+      </div>
+
+    )
+  }
+}
+
+FormSnippet.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  activeTab: React.PropTypes.string.isRequired,
+}
