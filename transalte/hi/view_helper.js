937273ac823ca49abb9538da2c57bde3995f2dde
Sanketkumar Karve <SanketKarve@users.noreply.github.com>
2021-03-14 12:13:37 +0530

    New translations view_helper.js (Hindi)

---
+++ transalte/hi/view_helper.js
@@ -0,0 +1,12 @@
+function getCSRFToken() {
+    return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+}
+function isIdPresentInDom(id) {
+    return document.getElementById(id);
+}
+$(document).ready(function () {
+    // helper to close flash messages
+    $('.message .close').on('click', function () {
+        $(this).closest('.message').hide();
+    });
+});
