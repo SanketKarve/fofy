1a57af6c1ad5c41f6403af0ad8e000ba0ffb7a48
Sanketkumar Karve <SanketKarve@users.noreply.github.com>
2021-03-14 12:16:08 +0530

    New translations forms_controller.rb (English)

---
+++ transalte/en/forms_controller.rb
@@ -0,0 +1,31 @@
+class FormsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @forms = Form.all
+    render json: @forms
+  end
+
+  def new
+    @form = Form.new
+    @fields = Field.new
+		respond_to do |format|
+			format.html
+		end
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
